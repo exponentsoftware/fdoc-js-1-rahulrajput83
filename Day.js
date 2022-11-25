6ed23c3b10fdc0bedeb9a16e753fca7a5f18f046
Rahul Rajput <rahulrajput21112@gmail.com>
2022-11-25 14:35:26 +0530

    2.c

--- Day.js
+++ Day.js
@@ -99,7 +99,7 @@ const unique = () => {
 console.log(unique())
 
 
-/* Reversing an array: Arrays have a reverse method which changes the array by inverting the order in which its elements appear. For this exercise, write a function, reverseArray. The  reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. Without reverse method. */
+/* 2.c Reversing an array: Arrays have a reverse method which changes the array by inverting the order in which its elements appear. For this exercise, write a function, reverseArray. The  reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. Without reverse method. */
 
 const reverseArray = (arr) => {
     let Array = [];
